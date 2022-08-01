ee9f4f1dd9483bcfc2a4a43e6000da5184edea21
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-08-01 13:58:54 +0900

    Fix: フッターの位置修正
    Fix: コンテンツ登録失敗した時フォームが縦に並ぶのを修正

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Towerbreaker
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
